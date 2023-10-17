75b19eb929e2663ee538a5bf9c151954d111556e
joeveloper <bebosir@gmail.com>
2023-10-17 20:25:06 +0200

    fix like button and count

---
+++ src/component/Button.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Button = () => {
+
+    const [count, setCount] = useState(100);
+    const [like, setLike] = useState(false);
+    const [text, setText] = useState("Like");
+
+    const handleClick = () => {
+        if (like) {
+            setCount(count - 1);
+            setLike(false);
+            setText("Like");
+          } else {
+            setCount(count + 1);
+            setLike(true);
+            setText("Liked");
+          }
+}
+
+
+    return (
+        <div>
+            <button onClick={()=> handleClick()}>{text}</button>
+            <span id='text'>{count}</span>
+        </div>
+
+    );
+}
+
+export default Button;
